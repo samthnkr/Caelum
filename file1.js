95f96b450c786460e43105f03d612610411869e2
Sameer Khan <88125436+samthnkr@users.noreply.github.com>
2023-08-06 15:42:04 +0530

    Add files via upload
    na

---
+++ file1.js
@@ -0,0 +1,43 @@
+      const video = document.getElementById('myVideo');
+
+      // Fetch data from JSON file and populate the HTML
+      fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+          const overlays = data.overlays.map(overlayData => {
+            const image = document.createElement('img');
+            image.src = overlayData.src;
+            image.classList.add('image-overlay');
+            image.dataset.start = overlayData.start;
+            image.dataset.end = overlayData.end;
+
+            const link = document.createElement('a');
+            link.href = overlayData.link;
+            link.target = '_blank';
+            link.appendChild(image);
+
+            document.getElementById('videoContainer').appendChild(link);
+
+            return {
+              ele: image,
+              element: link,
+              start: overlayData.start,
+              end: overlayData.end
+            };
+          });
+
+          // Now, add the event listener for timeupdate
+          video.addEventListener('timeupdate', function() {
+            const currentTime = video.currentTime;
+            overlays.forEach(overlay => {
+              const start = parseInt(overlay.start);
+              const end = parseInt(overlay.end);
+              if (currentTime >= start && currentTime <= end) {
+                overlay.ele.style.display = 'inline';
+              } else {
+                overlay.ele.style.display = 'none';
+              }
+            });
+          });
+        });
+    
